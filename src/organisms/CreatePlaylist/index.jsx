0e6524173c5936426d2f57426bc88fa71c7570c1
Joe Boylson <joeboylson@gmail.com>
2021-08-15 21:44:28 -0400

    improve CreatePlaylist;

--- src/organisms/CreatePlaylist/index.jsx
+++ src/organisms/CreatePlaylist/index.jsx
@@ -1,12 +1,11 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { get, post } from "axios";
 
-import tracks from '../Recommendations/data.json';
+import tracks from "../Recommendations/data.json";
 import { isEmpty } from "lodash";
 
 const decodeHash = (hash) => {
-
-  if (isEmpty(hash)) return null
+  if (isEmpty(hash)) return null;
 
   const _hash = hash.substring(1);
 
@@ -23,44 +22,55 @@ const decodeHash = (hash) => {
   return hashObj;
 };
 
-const createPlaylistFromLocalStorage = () => {
-  const hash = decodeHash(location.hash);
-  const tracks = window.localStorage.getItem('tracks')
-  const playlist = window.localStorage.getItem('playlist')
-
-  if (!hash || !tracks || !playlist) return null;
-
-  const tracksObj = JSON.parse(tracks)
-  const playlistObj = JSON.parse(playlist);
-  const trackUris = tracksObj.map(t => t.uri);
-
-  get(`https://api.spotify.com/v1/me?access_token=${hash.access_token}`).then(
-    (userData) => {
-      const user = userData.data;
-      post(
-        `https://api.spotify.com/v1/users/${user.id}/playlists?access_token=${hash.access_token}`,
-        playlistObj
-      ).then(playlistData => {
-        const playlist = playlistData.data
-        post(
-          `https://api.spotify.com/v1/playlists/${playlist.id}/tracks?access_token=${hash.access_token}`,
-          { uris: trackUris }
-        )
-      })
-    }
-  );
-
-  // console.log("SUCCESSFULLY CREATED PLAYLIST")
-  // window.localStorage.removeItem('tracks')
-  // window.localStorage.removeItem('playlist')
-};
-
 const CreatePlaylist = () => {
+  const [playlist, setPlaylist] = useState(null);
+
+  const createPlaylistFromLocalStorage = () => {
+    const hash = decodeHash(location.hash);
+    const tracks = window.localStorage.getItem("tracks");
+    const playlist = window.localStorage.getItem("playlist");
+
+    if (!hash) return console.log("NO HASH");
+    if (!tracks) return console.log("NO TRACKS");
+    if (!playlist) return console.log("NO PLAYLIST");
+
+    const tracksObj = JSON.parse(tracks);
+    const playlistObj = JSON.parse(playlist);
+    const trackUris = tracksObj.map((t) => t.uri);
+
+    get(`https://api.spotify.com/v1/me?access_token=${hash.access_token}`).then(
+      (userData) => {
+        const user = userData.data;
+        post(
+          `https://api.spotify.com/v1/users/${user.id}/playlists?access_token=${hash.access_token}`,
+          playlistObj
+        ).then((playlistData) => {
+          const playlist = playlistData.data;
+          post(
+            `https://api.spotify.com/v1/playlists/${playlist.id}/tracks?access_token=${hash.access_token}`,
+            { uris: trackUris }
+          ).then( result => {
+            setPlaylist(result)
+          })
+        });
+      }
+    );
+
+    window.localStorage.removeItem("tracks");
+    window.localStorage.removeItem("playlist");
+  };
+
   return (
     <div>
-      <button onClick={createPlaylistFromLocalStorage}>CREATE PLAYLIST</button>
+      {playlist ? (
+        <p>{JSON.stringify(playlist)}</p>
+      ) : (
+        <button onClick={createPlaylistFromLocalStorage}>
+          CREATE PLAYLIST
+        </button>
+      )}
     </div>
-  )
+  );
 };
 
 export default CreatePlaylist;
